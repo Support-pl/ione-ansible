862f15a5d07fc93809bda0fdedcbeaf42180507e
slntopp <slnt_opp@pm.me>
2018-12-06 17:45:44 +0300

    New executor logic and AnsiblePlaybookProcess
    New class and table for storing run processes

--- db.rb
+++ db.rb
@@ -2,8 +2,8 @@ require 'mysql2'
 
 # Get this values from /etc/oned.conf
 $db = {
-    :username => , :password => , 
-    :host => , :database => 'opennebula' }
+    :username => 'root', :password => 'opennebula', 
+    :host => 'localhost', :database => 'opennebula' }
 
 =begin
  CREATE TABLE ansible_playbook (
@@ -141,3 +141,205 @@ class AnsiblePlaybook
         me
     end
 end
+
+=begin
+ CREATE TABLE ansible_playbook_process (
+   proc_id INT NOT NULL AUTO_INCREMENT UNIQUE,
+   uid INT NOT NULL,
+   playbook_id INT NOT NULL,
+   install_id VARCHAR(128) NOT NULL UNIQUE,
+   create_time INT NOT NULL,
+   start_time INT,
+   end_time INT,
+   status VARCHAR(12),
+   log TEXT,
+   hosts TEXT NOT NULL,
+   vars TEXT NOT NULL,
+   PRIMARY KEY (proc_id) )
+=end
+
+class AnsiblePlaybookProcess
+
+    FIELDS  = %w(uid playbook_id install_id create_time start_time end_time status log hosts vars)
+    TABLE   = 'ansible_playbook_process'
+    
+    STATUS = {
+        '0' => 'PENDING',
+        '1' => 'RUNNING',
+        'ok' => 'SUCCESS',
+        'changed' => 'CHANGED',
+        'unreachable' => 'UNREACHABLE',
+        'failed' => 'FAILED',
+        '6' => 'LOST'
+    }
+
+    def initialize proc_id:nil, playbook_id:nil, uid:nil, hosts:[], vars:{}, auth:'default'
+        if proc_id.nil? then
+            @uid, @playbook_id = uid, playbook_id
+            @install_id = SecureRandom.uuid + '-' + Date.today.strftime
+            @create_time, @start_time, @end_time = Time.now.to_i, -1, -1
+            @status = '0'
+            @log = nil
+            @hosts = hosts
+            @vars = vars
+        else
+            @id = proc_id
+            sync
+        end
+        @playbook = AnsiblePlaybook.new(id: @playbook_id)
+        @service, @runnable = @playbook.runnable(@vars).to_a[0]
+    ensure
+        allocate if @id.nil?
+    end
+    
+    def run
+        nil if AnsiblePlaybookProcess::STATUS.keys.index(@status) > 0
+        @start_time, @status = Time.now.to_i, '1'
+        Thread.new do
+            begin
+                Net::SSH.start( ANSIBLE_HOST, ANSIBLE_HOST_USER, :port => ANSIBLE_HOST_PORT ) do | ssh |
+                    # Create local Playbook version
+                    File.open("/tmp/#{@install_id}.yml", 'w') do |file|
+                        file.write(
+                            @runnable.gsub('<%group%>', @install_id) )
+                    end
+                    # Upload Playbook to Ansible host
+                    ssh.sftp.upload!("/tmp/#{@install_id}.yml", "/tmp/#{@install_id}.yml")
+                    # Create local Hosts File
+                    File.open("/tmp/#{@install_id}.ini", 'w') do |file|
+                        file.write("[#{@install_id}]\n")
+                        @hosts.each {|host| file.write("#{host}\n") }
+                    end
+                    # Upload Hosts file
+                    ssh.sftp.upload!("/tmp/#{@install_id}.ini", "/tmp/#{@install_id}.ini")
+                    # Creating run log
+                    ssh.exec!("echo 'START' > /tmp/#{@install_id}.runlog")
+                    # Run Playbook
+                    ssh.exec!(
+                        "ansible-playbook /tmp/#{@install_id}.yml -i /tmp/#{@install_id}.ini >> /tmp/#{@install_id}.runlog; echo 'DONE' >> /tmp/#{@install_id}.runlog" )
+                
+                    @end_time = Time.now.to_i
+                    clean
+                    scan
+                end
+            rescue
+                @status = 'failed'
+            ensure
+                update
+            end
+        end
+    ensure
+        update
+    end
+
+    def scan
+        return nil if AnsiblePlaybookProcess::STATUS.keys.index(@status) > 1
+        Net::SSH.start( ANSIBLE_HOST, ANSIBLE_HOST_USER, :port => ANSIBLE_HOST_PORT ) do | ssh |
+            ssh.sftp.download!("/tmp/#{@install_id}.runlog", "/tmp/#{@install_id}.runlog")
+            @log = File.read("/tmp/#{@install_id}.runlog")
+            if @log.split(/\n/)[-1] == 'DONE' then
+                ssh.sftp.remove("/tmp/#{@install_id}.runlog")
+                @log.slice!("START\n")
+                @log.slice!("\nDONE\n")
+            else
+                @log = nil
+                return
+            end
+        end if @log == nil
+
+        codes = {}
+        
+        @log.split('PLAY RECAP').last.split(/\n/).map do | host |
+            host = host.split("\n").last.split(" ")
+            next if host.size == 1
+            codes.store host[0], {}
+            host[-4..-1].map do |code|
+                code = code.split("=")
+                codes[host[0]].store(code.first, code.last.to_i)
+            end
+        end
+        
+        if codes.values.inject(0){|sum, codes| sum +=  codes['failed']} != 0 then
+            @status = 'failed'
+        elsif codes.values.inject(0){|sum, codes| sum +=  codes['unreachable']} != 0 then
+            @status = 'unreachable'
+        else
+            @status = codes.values.last.keys.map do | key |
+                { key => codes.values.inject(0){|sum, codes| sum +=  codes[key]} }
+            end.sort_by{|attribute| attribute.values.last }.last.keys.last
+        end
+        
+        @codes = codes
+    rescue => e
+        puts e.message, e.backtrace
+        @status = '6'
+    ensure
+        update
+    end
+    def status
+        STATUS[@status]
+    end
+    def to_hash
+        db do |db|
+            db.query( "SELECT * FROM #{TABLE} WHERE proc_id=#{@id}" ).to_a.last
+        end
+    end
+
+    def self.list
+        result = db do |db| 
+            db.query( "SELECT * FROM #{TABLE}" ).to_a
+        end
+        result
+    end
+    
+    private
+
+    def clean
+        Net::SSH.start( ANSIBLE_HOST, ANSIBLE_HOST_USER, :port => ANSIBLE_HOST_PORT ) do | ssh |
+            ssh.sftp.remove!("/tmp/#{@install_id}.ini")
+            File.delete("/tmp/#{@install_id}.ini")
+            ssh.sftp.remove!("/tmp/#{@install_id}.yml")
+            File.delete("/tmp/#{@install_id}.yml")
+            ssh.sftp.remove("/tmp/#{@install_id}.retry")
+        end
+        nil
+    end
+    def update
+        db do |db|
+            FIELDS.each do | key |
+                db.query( "UPDATE #{TABLE} SET #{key}=#{
+                    var = instance_variable_get ('@' + key).to_sym
+                    var = JSON.generate var if var.class == Array || var.class == Hash
+                    var = "'#{var}'" if var.class == String
+                    var = 'null' if var.nil?
+                    var
+                } WHERE proc_id=#{@id}" )
+            end
+        end
+        nil
+    end
+    def allocate
+        db do | db |
+            db.query(
+                "INSERT INTO #{TABLE} (#{FIELDS.join(', ')}) VALUES (#{
+                    FIELDS.map do |var|
+                        var = instance_variable_get(('@' + var).to_sym)
+                        var = JSON.generate var if var.class == Array || var.class == Hash
+                        var = "'#{var}'" if var.class == String
+                        var = 'null' if var.nil?
+                        var
+                    end.join(', ')})"
+            )
+            @id = db.query( "SELECT proc_id FROM #{TABLE}" ).to_a.last['proc_id']
+        end
+    end
+    def sync
+        db do |db|
+            db.query( "SELECT * FROM #{TABLE} WHERE proc_id=#{@id}" ).to_a.last.each {|key, value| instance_variable_set(('@' + key).to_sym, value)}
+        end
+        @hosts = JSON.parse @hosts
+        @vars = JSON.parse @vars
+    end
+end
+
+app = AnsiblePlaybookProcess.new playbook_id:23, uid:0, hosts:['185.66.68.11:52222', '185.66.68.114:22', '185.66.69.206:52222'], vars:{'cause_error' => 'false', 'work_time' => 5}, auth:'default'
